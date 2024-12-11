5ed63304a9d673f910267624ae05e83db8212d03
hda <d.harke@yandex.ru>
2024-12-11 10:13:40 +0300

    check auth

--- src/router/AuthGuard.tsx
+++ src/router/AuthGuard.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { publicRoutePaths } from ".";
 
